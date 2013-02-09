43a5793da69caca818d0cd28588acdfe7cb088ce
lmparra <lfmparra@gmail.com>
2013-02-09 11:26:30 -0500

    Se resuleve un conflicto

--- src/edu/ws/UserServerImpl.java
+++ src/edu/ws/UserServerImpl.java
@@ -1,11 +1,6 @@
 package edu.ws;
 
-<<<<<<< HEAD
-//Service Implementation Bean
 
-import com.sun.jmx.remote.internal.ArrayQueue;
-=======
->>>>>>> a06d37d0578826ca03ae67f69d484cbaf98e6794
 import edu.logic.DBConnector;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -31,11 +26,7 @@ public class UserServerImpl implements UserServer {
     private PreparedStatement pstmt;
     private ResultSet result;
     private String passField;
-<<<<<<< HEAD
     private ArrayDeque userdata;
-=======
-    private ArrayDeque<String> userdata;
->>>>>>> a06d37d0578826ca03ae67f69d484cbaf98e6794
     private List<String> stringFields, intFields;
 
     @Override
